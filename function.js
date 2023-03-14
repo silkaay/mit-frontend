265c9219b621f8ba503a485dd1e7543d1b501d8f
silkaay <114661229+silkaay@users.noreply.github.com>
2023-03-14 23:20:31 +0100

    added overlay to form background

--- function.js
+++ function.js
@@ -1,7 +1,9 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("form-overlay").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("form-overlay").style.display = "none";
+}
