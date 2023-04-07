62d0fa6eff63b20b930a5fd6daa79926bad18343
silkaay <silke.pajenkamp@gmail.com>
2023-04-07 20:11:12 +0200

    user entries are retrieved

--- blog.js
+++ blog.js
@@ -12,11 +12,21 @@ function submitForm() {
   location.reload();
 }
 
+//Hier zieht er sich die Posts und schickt sie an das create
+fetch("http://localhost:8080/getAllBlogposts")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(blogpost => {
+      createBlogpost(blogpost.blogpostId, blogpost.blogpostAuthor, blogpost.blogpostTitle, blogpost.blogpostCreationDate, blogpost.blogpostText, blogpost.blogpostJourneyId, blogpost.blogpostPOIId);
+    });
+  })
+  .catch(error => console.error(error));
 
-//Display the Blogposts
+
+//Liste aus der HTML, dass sie in die DOM geadded werden können
 const bloglist = document.getElementById("bloglist");
 
-
+//obvious, aber hier werden die Posts generiert
 function createBlogpost(blogpostId, blogpostAuthor, blogpostTitle, blogpostCreationDate, blogpostText, blogpostJourneyId, blogpostPOIId) {
   const blog = document.createElement("div");
   blog.className = "blogpost";
@@ -28,16 +38,55 @@ function createBlogpost(blogpostId, blogpostAuthor, blogpostTitle, blogpostCreat
         <h6></h6>
       </div>
       <p>${blogpostText}</p>
-      <button type="submit">Edit</button>
+      <button type="submit" onclick="editBlogpost(${blogpostId}, this)">Edit</button>
       <button type="submit" onclick="deleteBlogpost(${blogpostId}, this)">Delete</button>
     </div>
   `;
   bloglist.appendChild(blog);
 }
 
+
+function postBlogpost() {
+  var form = document.forms["myForm"];
+
+  // get the values of the input elements
+  var title = form.title.value;
+  var entrydate = form.entrydate.value;
+  var author = form.author.value;
+  var blogtext = form.blogtext.value;
+  /*
+  var entrydate = form.elements["entrydate"].value;
+  var author = form.elements["author"].value;
+  var blogtext = form.elements["blogtext"].value;*/
+
+  console.log(title, entrydate, author, blogtext);
+}
+
 /* Diese Line wurde aus dem inner html entfern, hier nur zum Sichern
 <p>${blogpostJourneyId}, ${blogpostPOIId}</p>
 */
+
+//Bearbeite einzelne Blogposts
+function editBlogpost (blogpostId, button) {
+  //öffnet die Form
+  document.getElementById("form-overlay").style.display = "block";
+  document.getElementById("editForm").style.display = "block";
+
+
+  console.log("test");
+}
+
+function openBlog() {
+  document.getElementById("form-overlay").style.display = "block";
+  document.getElementById("editForm").style.display = "block";
+}
+
+function closeBlog() {
+  document.getElementById("editForm").style.display = "none";
+  document.getElementById("form-overlay").style.display = "none";
+}
+
+//Lösche die einzelnen Blogposts
 function deleteBlogpost(blogpostId, button) {
   // Sende DELETE-Request an den Server
   fetch(`http://localhost:8080/deleteBlogpost/${blogpostId}`, {
@@ -54,17 +103,7 @@ function deleteBlogpost(blogpostId, button) {
   .catch(error => console.error(`Error deleting blogpost with id ${blogpostId}: ${error}`));
 }
 
-
-fetch("http://localhost:8080/getAllBlogposts")
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(blogpost => {
-      createBlogpost(blogpost.blogpostId, blogpost.blogpostAuthor, blogpost.blogpostTitle, blogpost.blogpostCreationDate, blogpost.blogpostText, blogpost.blogpostJourneyId, blogpost.blogpostPOIId);
-    });
-  })
-  .catch(error => console.error(error));
-
-
+//Ab hier sind die Dropdown-Felder für die erstellen Ansicht
 function journeyDropdown() {
   fetch("http://localhost:8080/getSelectionJourneys")
     .then(response => response.json())
